f227413d61a28bd79cf67810ed01c1bf54af78e8
Társis  N. B. da Silva <tarsis.silva@ecomp.ufsm.br>
2021-04-08 12:52:01 -0300

    bug x axis solved
    - implementada a plotagem do sinal de (pyaudio) em x e y (domínio do tempo)
    * corrigir problema de processamento para janelas "grandes"
    *plot iniciando no fim da janela (array y não inicializado com valores relevantes)

--- main.py
+++ main.py
@@ -20,93 +20,6 @@ from PyQt5 import uic
 import time
 import queue 
 import pyaudio
-# not in use
-class Ploter():
-
-	def __init__(self):
-		
-		#self.sample_rate = 8000
-		#self.t_sample = 1/self.sample_rate #criar método método 
-		#self.total_samples  = int(self.window*self.sample_rate/(1000))
-		self.q = queue.Queue()
-		self.samp_rate = 8000
-		self.samp_step = 1/self.samp_rate
-		self.window = 40000
-		self.window_t = self.window * self.samp_step
-		self.time_interval = 10
-
-		self.x = np.arange(0 , self.window_t , self.samp_step, dtype = float)
-		self.y = np.zeros(self.window, dtype = float)
-
-		self.fig,self.ax = plt.subplots(figsize=(8,4))
-		self.ax.set_title("plot")
-		self.ax.set_facecolor((1,1,1))
-		self.ax.set_ylim(-1.1, 1.1)
-		self.ax.set_xlim(0, self.window)
-		self.ax.grid()
-
-		self.fig,self.bx = plt.subplots(figsize=(8,4))
-		self.bx.set_title("plot")
-		self.bx.set_facecolor((1,1,1))
-		self.bx.set_ylim(-1.1, 1.1)
-		self.bx.set_xlim(0, self.window_t)
-		self.bx.grid()
-
-		print("objeto Ploter instanciado : area de plot criada")
-	
-		
-	def update_plt_list(self, i):
-		
-		data = np.array(self.get_list())/32768
-		shift = len(data)
-
-		self.y = np.roll(self.y,-shift)
-		self.y[-shift:] = data
-		xmin, xmax = self.ax.get_xlim()
-		self.x = np.roll(self.x,-shift)
-		self.x[-shift:] = np.arange(xmax,xmax + shift)
-		print(self.x)
-		
-		
-		self.ax.set_xlim(xmin + shift ,xmax + shift)
-		self.ax.figure.canvas.draw()	
-
-		self.lines = self.ax.plot(self.x, self.y, color = (0,0,0), lw=0.5)
-		return(self.lines)
-	
-
-
-	def update_plt_list_time(self, i):
-		#get samples an frame size
-		data = np.array(self.get_list())/32768
-		shift = len(data)
-		
-		#refresh y axis
-		self.y = np.roll(self.y,-shift)
-		self.y[-shift:] = data
-		
-		#refresh x axis
-		xmin, xmax = self.bx.get_xlim()
-		self.x = np.roll(self.x,-shift)
-		self.x[-shift:] = np.arange(xmax,xmax + shift*self.samp_step, self.samp_step)
-		
-		#refresh window plot limits
-		self.bx.set_xlim(xmin + shift*self.samp_step ,xmax + shift*self.samp_step)
-		self.bx.figure.canvas.draw()	
-		
-		#put all in to lines object
-		self.lines = self.bx.plot(self.x, self.y, color = (0,0,0), lw=0.5)
-		
-		return(self.lines)
-
-
-	def get_list(self): 
-		data = self.q.get() 
-	
-		return data
-
-	def put_list(self, thing):
-		self.q.put(thing)
         
 class MplCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -125,17 +38,17 @@ class PloterMpl(QtWidgets.QMainWindow):
         #plot props just copied, need check*
         self.samp_rate = 8000
         self.samp_step = 1/self.samp_rate
-        self.window = 8000
+        self.window = 160000
         self.window_t = self.window * self.samp_step
         self.time_interval = 10 
 
         #define np arrays data x and y
         self.x = np.arange(0 , self.window_t , self.samp_step, dtype = float)
         self.y = np.zeros(self.window, dtype = float)
-        
+        self.plotdata = np.zeros(self.window, dtype = float)
         #define fig to plot
-        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
-        
+        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)   
+
         #timer plot refresh interval (ms)
         self.interval = 30
 
@@ -160,7 +73,7 @@ class PloterMpl(QtWidgets.QMainWindow):
         self.timer.setInterval(self.interval) #msec
         self.timer.timeout.connect(self.update_plt_list)
         self.timer.start()
-
+        self.reference_plot = None
         self.p = pyaudio.PyAudio()
         
         self.pushButton.clicked.connect(self.start_worker)
@@ -180,8 +93,8 @@ class PloterMpl(QtWidgets.QMainWindow):
         self.worker = Worker(self.start_audio, )
         self.threadpool.start(self.worker)	
         
-        #self.reference_plot = None
-        #self.timer.setInterval(self.interval) 
+        self.reference_plot = None
+        self.timer.setInterval(self.interval) 
 
     def new_method(self):
          self.lineEdit.setEnabled(False)#msec
@@ -220,32 +133,47 @@ class PloterMpl(QtWidgets.QMainWindow):
         self.p.terminate()
 
     def update_plt_list(self):
-        print('ACTIVE THREADS:',self.threadpool.activeThreadCount(),end=" \r")
-        while True:
-            QtWidgets.QApplication.processEvents()
-            try: 
-                #self.data = self.q.get_nowait()
-                data = np.array(self.get_list())/32768
+        try:
+            #print('ACTIVE THREADS:',self.threadpool.activeThreadCount(),end=" \r")
+            
+            while True:
+            
+                QtWidgets.QApplication.processEvents()
+            
+                try: 
+            
+                    #self.data = self.q.get_nowait()
+            
+                    data = np.array(self.q.get_nowait())/32768
 
-            except queue.Empty:
-                break
+
             
-            shift = len(data)
+                except queue.Empty:
             
-            self.canvas.axes.set_facecolor((0,0,0))
-            self.y = np.roll(self.y,-shift) 
-            self.y[-shift:] = data  
-            #self.y = data[:]
-            self.canvas.axes.set_facecolor((1,1,1))
-            self.canvas.axes.plot( self.y, color=(0,1,0.29))
-        print(len(self.y))
-        self.canvas.axes.set_ylim( ymin=-1, ymax=1)
-        self.canvas.axes.yaxis.grid(True,linestyle='--')
-        start, end = self.canvas.axes.get_ylim()
-        self.canvas.axes.yaxis.set_ticks(np.arange(start, end, 0.1))
-        self.canvas.axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
-        self.canvas.axes.set_ylim( ymin=-0.5, ymax=0.5)	
-        self.canvas.draw()
+                    break
+                shift = len(data)
+                self.canvas.axes.set_facecolor((0,0,0))
+                self.plotdata = np.roll(self.plotdata,-shift) 
+                self.plotdata[-shift:] = data
+                self.y = self.plotdata[:]
+                xmin, xmax = self.canvas.axes.get_xlim()
+                self.x = np.roll(self.x,-shift)
+                self.x[-shift:] = np.arange(xmax,xmax + shift* self.samp_step, self.samp_step)
+
+                #self.y = data[:]
+                self.canvas.axes.set_facecolor((1,1,1))
+                self.canvas.axes.set_xlim(xmin + shift*self.samp_step ,xmax + shift*self.samp_step)
+                
+
+            self.canvas.axes.plot(self.x, self.y, color=(0,1,0.29))
+            self.canvas.axes.set_ylim( ymin=-1, ymax=1)
+            self.canvas.axes.yaxis.grid(True,linestyle='--')
+            #self.canvas.axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
+            
+            self.canvas.draw()
+        except Exception as e:
+            print("Error:",e)
+        pass
 
 
 
