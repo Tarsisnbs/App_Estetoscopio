f8cccc8b52e5d61e49948ccbf6d53711aba9764b
Társis  N. B. da Silva <tarsis.silva@ecomp.ufsm.br>
2021-04-14 17:34:10 -0300

    Armazenamento
    Implementada a funcionalidade de salvar e plotar todo o sinal registrado no termino do processo de gravação.

--- main.py
+++ main.py
@@ -14,19 +14,68 @@ from matplotlib.figure import Figure
 import matplotlib.ticker as ticker
 import numpy as np
 from PyQt5.QtCore import pyqtSlot
-from matplotlib.animation import FuncAnimation
-from PyQt5 import QtCore, QtWidgets,QtGui
+from PyQt5 import QtCore, QtWidgets
 from PyQt5 import uic
-import time
 import queue 
 import pyaudio
+import soundfile as sf
+
+class Ploter (FigureCanvas):
+    
+    def __init__(self, window_t, samp_step, parent=None, width=5, height=4, dpi=100 ):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(Ploter, self).__init__(fig)
+        #plot props just copied, need check*
+        self.samp_step = 1/8000
+        self.window_t = 2
+        self.window = 16000
+        self.axes.set_xlim(0, self.window_t)
+        self.time_interval = 30 
+
+        #define np arrays data x and y
+        self.x = np.arange(0 , self.window_t , self.samp_step, dtype = float)
+        self.y = np.zeros(self.window, dtype = float)
+        fig.tight_layout()
+    
+    pass
+    def plot_signal(self, signal, samp_step):
+        self.axes.clear()
+        self.axes.plot(np.arange(0,len(signal)*samp_step,samp_step), signal, color = (0,1,0), lw=0.5)
+        self.draw()
         
-class MplCanvas(FigureCanvas):
-	def __init__(self, parent=None, width=5, height=4, dpi=100):
-		fig = Figure(figsize=(width, height), dpi=dpi)
-		self.axes = fig.add_subplot(111)
-		super(MplCanvas, self).__init__(fig)
-		fig.tight_layout()
+    def plot_real_time (self,data, samp_step, window_t):
+        shift = len(data)
+        
+        xmin, xmax = self.axes.get_xlim()
+        
+        if( self.x[0] < window_t): 
+            self.y[:shift] = data
+            print("buffering x:", self.x[0])
+        else: 
+            self.y = np.roll(self.y,-shift)
+            self.y[-shift:] = data
+            self.axes.set_xlim(xmin + shift*samp_step ,xmax + shift*samp_step)
+        
+        self.x = np.roll(self.x,-shift)
+        self.x[-shift:] = np.arange(xmax,xmax + shift* self.samp_step, self.samp_step)
+        
+        self.axes.set_facecolor((1,1,1))
+        self.axes.set_ylim( ymin=-1, ymax=1)
+        self.axes.plot(self.x, self.y, color = (0,1,0), lw=0.5)
+        print(self.x)
+        self.draw()       
+
+class Registrador(): 
+    def __init__(self):
+        pass
+    def save_wav(audio_array): 
+        sf.write('stereo_file.wav', audio_array, 8000, 'PCM_24') 
+
+    def load_wav(): 
+        pass
+    
+    pass
 
 class PloterMpl(QtWidgets.QMainWindow):
     WIDTH = 2
@@ -38,17 +87,11 @@ class PloterMpl(QtWidgets.QMainWindow):
         #plot props just copied, need check*
         self.samp_rate = 8000
         self.samp_step = 1/self.samp_rate
-        self.window = 80000
+        self.window = 16000
         self.window_t = self.window * self.samp_step
         self.time_interval = 30 
-
-        #define np arrays data x and y
-        self.x = np.arange(0 , self.window_t , self.samp_step, dtype = float)
-        self.y = np.zeros(self.window, dtype = float)
-        #define fig to plot
-        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)   
-        self.canvas.axes.set_xlim(0, self.window_t)
-        #timer plot refresh interval (ms)
+        
+        self.canvas = Ploter(self, self.samp_step, self.window_t, width=5, height=4, dpi=100)
         self.interval = 30
 
         #define new Qthread for pyaudio
@@ -57,10 +100,10 @@ class PloterMpl(QtWidgets.QMainWindow):
 
         #buffer
         self.q = queue.Queue()
-        
         self.plot_now = False
         
-        
+        #self.audio_array = np.arange(0, self.audio_size*self.samp_rate, dtype = float)
+        self.audio_list = []
         QtWidgets.QMainWindow.__init__(self)
         self.resize(1024, 600)
         self.ui = uic.loadUi('main.ui', self)
@@ -118,8 +161,9 @@ class PloterMpl(QtWidgets.QMainWindow):
             print("ERROR: ",e)
             pass
 
-
     def stop_stream(self):
+        sf.write('audio_file.wav', np.array(self.audio_list)/32768, 8000) 
+        self.canvas.plot_signal(np.array(self.audio_list)/32768, self.samp_step)
         self.stream.stop_stream()
         self.stream.close()
         self.p.terminate()
@@ -133,34 +177,14 @@ class PloterMpl(QtWidgets.QMainWindow):
                 QtWidgets.QApplication.processEvents()
             
                 try: 
-                     data = np.array(self.q.get_nowait())/32768
+                    data_list = self.q.get_nowait()
+                    data = np.array(data_list)/32768
 
                 except queue.Empty:
             
                     break
-                shift = len(data)
-        
-                xmin, xmax = self.canvas.axes.get_xlim()
-                if( self.x[0] < self.window_t): 
-                    self.y[:shift] = data
-                    print("buffering x:", self.x[0])
-                else: 
-                    self.y = np.roll(self.y,-shift)
-                    self.y[-shift:] = data
-                    self.canvas.axes.set_xlim(xmin + shift*self.samp_step ,xmax + shift*self.samp_step)
-
-                self.x = np.roll(self.x,-shift)
-                self.x[-shift:] = np.arange(xmax,xmax + shift* self.samp_step, self.samp_step)
-                
-                #self.y = data[:]
-                self.canvas.axes.set_facecolor((1,1,1))
-                
-                self.canvas.axes.plot(self.x, self.y, color = (0,1,0), lw=0.5)
-                self.canvas.axes.set_ylim( ymin=-1, ymax=1)
-           
-            #self.canvas.axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
-            
-            self.canvas.draw()
+                self.canvas.plot_real_time(data, self.samp_step, self.window_t)
+                self.audio_list.extend(data_list)
         except Exception as e:
             print("Error:",e)
         pass
